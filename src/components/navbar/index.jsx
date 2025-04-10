3d85ff1809ab655317675f15e178c582ded24909
akmalxonov <117194536+akmalxonov@users.noreply.github.com>
2025-04-10 06:36:08 +0500

    dars1-react.js

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../navbar/navbar.scss';
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+        </div>
+    );
+}
+
+export default Navbar;
